a81758801b216c342446371c980767655a9c9c9c
Robson Cristian de Lira <robso.crlira@gmail.com>
2020-12-06 02:43:27 -0300

    Finalizacao da questao1
    Questao 1 das atividades concluido

--- Questao1/questao1.py
+++ Questao1/questao1.py
@@ -1,18 +1,26 @@
 def shift(vetor, valor):
     vetor_new = []
-    x = 0
-    elementos = len(vetor)
-    while valor < elementos:
-        print(vetor[valor])
-        valor = valor + 1
-        elementos = elementos - 1
+    negativo = len(vetor) + valor
+    positivo = len(vetor) - valor
 
-    while valor > elementos:
-        print(vetor[valor])
+    while valor < negativo:
+        vetor_new.append(vetor[valor])
+        valor = valor + 1
+
+    while valor > positivo:
+        vetor_new.append(vetor[valor])
         valor = valor - 1
-        elementos = elementos + 1
+
+    return vetor_new
+
 
 if __name__ == "__main__":
 
     vet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    shift(vet, 1)
+
+    vet2 = shift(vet, -2)
+
+    for i in vet2:
+        print(i)
+
+
