023de1e036c7673f7f2466eca7a98b31ac47e297
Robson Cristian de Lira <robso.crlira@gmail.com>
2021-02-01 21:35:36 -0300

    Delete gerenciadorapi directory

--- gerenciadorapi/src/main/java/com/juntosseguros/gerenciadorapi/repositorio/CartaoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.juntosseguros.gerenciadorapi.repositorio;
-
-import com.juntosseguros.gerenciadorapi.entidade.Cartao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartaoRepositorio extends JpaRepository<Cartao, Long> {
-
-}
